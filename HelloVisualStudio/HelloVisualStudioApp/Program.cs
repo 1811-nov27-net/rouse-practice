2ab6121a6dbac86d8a975501f5f5b73e0e34e33b
devin-rouse <devin.a.rouse@gmail.com>
2018-11-28 11:58:19 -0600

    Create Hello Visual Studio App

---
+++ HelloVisualStudio/HelloVisualStudioApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloVisualStudioApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // F5, Ctrl+F5 to run your code
+            // File -> New Project to make new projects
+            // Do NOT create a new repo on project creation
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
