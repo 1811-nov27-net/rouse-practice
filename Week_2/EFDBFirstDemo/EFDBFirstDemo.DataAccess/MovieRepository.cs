958a06cdc46379d387a72c1914b0a753db47018b
devin-rouse <devin.a.rouse@gmail.com>
2018-12-12 10:20:18 -0600

    Update up to week 3 MVCDemo creation

---
+++ Week_2/EFDBFirstDemo/EFDBFirstDemo.DataAccess/MovieRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDBFirstDemo.DataAccess
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public MoviesDBContext Db { get; }
+
+        public IList<Movie> GetAllMovies()
+        {
+
+
+        }
+
+        public IList<Movie> GetAllMoviesWithGenres()
+        {
+
+        }
+
+        public Movie GetMovieById(int id)
+        {
+
+        }
+
+        public Movie GetMovieByName(string name)
+        {
+
+        }
+
+        public void DeleteMovie(Movie movie)
+        {
+
+        }
+
+        public void EditMovie(Movie movie)
+        {
+
+        }
+
+        public void CreateMovie(Movie movie)
+        {
+            Db.Add(movie);
+        }
+
+        public void SaveChanges()
+        {
+            Db.SaveChanges();
+        }
+    }
+}
---
+++ Week_2/EFDBFirstDemo/EFDBFirstDemo.UI/SecretConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDBFirstDemo.UI
+{
+    public static class SecretConfiguration
+    {
+        public static readonly string ConnectionString = "Server=tcp:rouse1811.database.windows.net,1433;Initial Catalog=MoviesDB;Persist Security Info=False;User ID=Rouse_D;Password=839A51bcEf881;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
